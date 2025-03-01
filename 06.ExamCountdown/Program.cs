f4f029c53c77cac9c81645556d881e815c4d4b45
rebelwithoutacause <148477548+rebelwithoutacause@users.noreply.github.com>
2025-03-01 10:55:09 +0200

    Add files via upload

---
+++ 06.ExamCountdown/Program.cs
@@ -0,0 +1,7 @@
+﻿int d = int.Parse(Console.ReadLine());
+
+for (int daysToExam = d; d >= 1; d--) 
+{
+Console.WriteLine($"{d} days before the exam");
+}
+Console.WriteLine("The exam has come");
---
+++ 07.LatinLetters/Program.cs
@@ -0,0 +1,9 @@
+﻿char start = char.Parse(Console.ReadLine());
+char end = char.Parse(Console.ReadLine());
+
+
+for (char letter = start; letter <= end; letter++)
+{
+    Console.Write(letter + " ");
+}
+Console.WriteLine();
