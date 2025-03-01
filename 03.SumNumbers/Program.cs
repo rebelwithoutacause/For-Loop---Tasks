c09383c486437a2d02e9a7c532435b092dfecbc2
rebelwithoutacause <148477548+rebelwithoutacause@users.noreply.github.com>
2025-03-01 10:52:57 +0200

    Add files via upload

---
+++ 03.SumNumbers/Program.cs
@@ -0,0 +1,9 @@
+﻿int numberCount = int.Parse(Console.ReadLine());
+
+double sum = 0;
+
+for (int i = 0; i < numberCount; i+=1) 
+{
+    sum += double.Parse(Console.ReadLine());
+}
+Console.WriteLine(sum);
